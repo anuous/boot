9199a39c3e9aa673779e3140600a53d806ddf839
luoyuanguang <anuous@aliyun.com>
2019-11-06 13:57:58 +0800

    pdf模板文件 域填充值，文件最后追加动态表格文件

--- src/main/java/com/anuous/boot/pdf/PDFController.java
+++ src/main/java/com/anuous/boot/pdf/PDFController.java
@@ -12,8 +12,9 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -39,6 +40,63 @@ public class PDFController {
              byte[] re = {};
             ByteArrayInputStream bis = new ByteArrayInputStream(re);
 
+
+            IOUtils.copy (fis, response.getOutputStream());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally{
+            if(fis!=null){
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(tempfilePath!=null && tempfilePath.length()>0){
+                File file = new File(tempfilePath);
+                if(file.isFile()){
+                    file.delete();
+                }
+            }
+        }
+    }
+
+    @RequestMapping(method= RequestMethod.GET,value = "/table")
+    public void createTable(HttpServletResponse response) throws  Exception{
+       // PpdfUtils.createTable();
+    }
+
+    @RequestMapping(method= RequestMethod.GET,value = "/fillTable")
+    public void testFillValueAndInsertTable(HttpServletResponse response){
+
+        response.setContentType(MediaType.APPLICATION_PDF_VALUE);
+        Map<String,String> paramValues = new HashMap<>(4);
+        paramValues.put("textArea-A","textArea-A:测试域 A企业");
+        paramValues.put("textArea-B","textArea-B:测试域 B企业");
+        paramValues.put("SignatureA","签名域");
+        String[] headers=new String[]{"发票号码","发票代码","开票日期","购货方","销货方（经销商）","发票金额"};
+        List<Invoice> invoices = new ArrayList<Invoice>();
+        Invoice invoice = new  Invoice("10001","1","20191029", "木森林股份有限公司","世界上最长的公司是我家是我家是我家是我家是我家是我家","100000");
+        Invoice invoice1 = new  Invoice("10002","2","20191029","木森林股份有限公司","世界上最长的公司是我家","100000");
+        Invoice invoice2 = new  Invoice("10003","3","20191029","木森林股份有限公司","世界上最长的公司是我家","100000");
+        Invoice invoice3 = new  Invoice("10004","4","20191029","木森林股份有限公司","世界上最长的公司是我家","100000100000");
+        invoices.add(invoice);
+        invoices.add(invoice1);
+        invoices.add(invoice2);
+        invoices.add(invoice3);
+        int res =0;
+        while(res<102){
+            Invoice tmp = new  Invoice("10001"+res,"1","20191029", "木森林股份有限公司","世界上最长的公司是我家是我家是我家是我家是我家是我家","100000");
+            invoices.add(tmp);
+            res++;
+        }
+        String tempfilePath ="";
+        FileInputStream fis = null;
+        try {
+            tempfilePath= PpdfUtils.fillValueAndInsertTables(headers,invoices,paramValues);
+            fis= new FileInputStream(tempfilePath);
+            byte[] re = {};
+            ByteArrayInputStream bis = new ByteArrayInputStream(re);
 
             IOUtils.copy (fis, response.getOutputStream());
         } catch (Exception e) {
--- src/main/java/com/anuous/boot/pdf/PDFUtils.java
+++ src/main/java/com/anuous/boot/pdf/PDFUtils.java
@@ -4,13 +4,19 @@ import com.itextpdf.forms.PdfAcroForm;
 import com.itextpdf.io.font.PdfEncodings;
 import com.itextpdf.kernel.font.PdfFont;
 import com.itextpdf.kernel.font.PdfFontFactory;
+import com.itextpdf.kernel.geom.PageSize;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfReader;
 import com.itextpdf.kernel.pdf.PdfWriter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Table;
+import com.itextpdf.layout.layout.LayoutArea;
+import com.itextpdf.layout.layout.LayoutResult;
+import com.itextpdf.layout.renderer.DocumentRenderer;
+import com.itextpdf.layout.renderer.IRenderer;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Map;
 
 @Component
@@ -26,7 +32,14 @@ public class PDFUtils {
 
     private String tempaltePath ="./src/main/resources/pdf/test-src.pdf";
 
-    private String srcPath ="./src/main/resources/pdf/desc/test-rest-4.pdf";
+    private String srcPath ="./src/main/resources/pdf/desc/test-desc-4.pdf";
+
+    private String desc ="./src/main/resources/pdf/desc/test-desc-4.pdf";
+
+    private String tmp1 ="./src/main/resources/pdf/desc/tmp1-desc-4.pdf";
+
+    private String tmp2 ="./src/main/resources/pdf/desc/tmp2-desc-4.pdf";
+
 
     public String fillSpecialValues(Map<String,String>paramValues) throws Exception {
        return fillSpecialValues(tempaltePath,srcPath,paramValues);
@@ -47,5 +60,95 @@ public class PDFUtils {
         return descPath;
     }
 
+
+    public String fillValueAndInsertTables(String[] headers,List<Invoice> invoices,Map<String,String>paramValues)throws Exception{
+        //1、根据PDF模板 填充域 生产新的PDF
+        PdfDocument tmpPdf1 = new PdfDocument(new PdfReader(tempaltePath), new PdfWriter(tmp1));
+        PdfAcroForm form = PdfAcroForm.getAcroForm(tmpPdf1, true);
+        form.setGenerateAppearance(true);
+        PdfFont font = PdfFontFactory.createFont(fontPath, PdfEncodings.IDENTITY_H);
+        for(String key : paramValues.keySet()){
+            form.getField(key).setValue(paramValues.get(key), font, fontSize);
+        }
+        tmpPdf1.close();
+        //2、生产表格临时PDF文件2
+        PdfDocument tmpPdf2 = new PdfDocument( new PdfWriter(tmp2));
+        Document doc = new Document(tmpPdf2, new PageSize(PageSize.A4));
+        //doc.setMargins(40, 30, 30, 30);
+        Table table = createTables(headers,invoices);
+        doc.add(table);
+        //doc.setRenderer(new DocumentRenderer(doc));
+        doc.close();
+        //3、PDF文件合并
+        PdfWriter writer = new PdfWriter(srcPath);
+        writer.setSmartMode(true);
+        PdfDocument pdfDoc = new PdfDocument(writer);
+        pdfDoc.initializeOutlines();
+        PdfDocument addedDoc1 = new PdfDocument(new PdfReader(tmp1));
+        addedDoc1.copyPagesTo(1, addedDoc1.getNumberOfPages(), pdfDoc);
+        addedDoc1.close();
+        PdfDocument addedDoc2 = new PdfDocument(new PdfReader(tmp2));
+        addedDoc2.copyPagesTo(1, addedDoc2.getNumberOfPages(), pdfDoc);
+        addedDoc2.close();
+
+        pdfDoc.close();
+        return srcPath;
+    }
+    /**
+     *创建PDF表格
+     * @param headers  表头
+     * @param invoices 表格数据
+     * @return
+     * @throws Exception
+     */
+    private Table createTables(String[]headers,List<Invoice> invoices) throws  Exception{
+        if(headers==null || headers.length ==0){
+            return null;
+        }
+        if(invoices == null || invoices.size()==0){
+            return null;
+        }
+        PdfFont font = PdfFontFactory.createFont(fontPath, PdfEncodings.IDENTITY_H);
+        Table table =new Table(new float[headers.length]);
+        for (int i = 0; i < headers.length; i++) {
+            table.addCell(headers[i]).setFont(font).setAutoLayout();
+        }
+        for (Invoice invoice:invoices) {
+            table.addCell(invoice.getA()).setFont(font).setAutoLayout();
+            table.addCell(invoice.getB()).setFont(font).setAutoLayout();
+            table.addCell(invoice.getC()).setFont(font).setAutoLayout();
+            table.addCell(invoice.getD()).setFont(font).setAutoLayout();
+            table.addCell(invoice.getE()).setFont(font).setAutoLayout();
+            table.addCell(invoice.getF()).setFont(font).setAutoLayout();
+        }
+        return table;
+    }
+
+
+    protected static class ExtraTableRenderer extends DocumentRenderer {
+
+        public ExtraTableRenderer(Document document) {
+            super(document);
+        }
+
+        // If renderer overflows on the next area, iText uses getNextRender() method to create a renderer for the overflow part.
+        // If getNextRenderer isn't overriden, the default method will be used and thus a default rather than custom
+        // renderer will be created
+        @Override
+        public IRenderer getNextRenderer() {
+            return new ExtraTableRenderer(document);
+        }
+
+        @Override
+        protected LayoutArea updateCurrentArea(LayoutResult overflowResult) {
+            LayoutArea area = super.updateCurrentArea(overflowResult);
+            if (area.getPageNumber() == 1) {
+                area.getBBox().decreaseHeight(266);
+            }
+
+            return area;
+        }
+
+    }
 
 }
